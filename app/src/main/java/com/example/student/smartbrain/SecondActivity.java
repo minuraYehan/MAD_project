119216874c0c15a0e71d70bff2d300e83c3ee41f
minuraYehan <minura.yehan99@gmail.com>
2019-08-06 13:08:34 +0530

    home,signup,easy-medium-hard

--- app/src/main/java/com/example/student/smartbrain/SecondActivity.java
+++ app/src/main/java/com/example/student/smartbrain/SecondActivity.java
@@ -5,15 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class FirstActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
+        setContentView(R.layout.activity_second);
     }
 
-    public void signupPage(View view) {
-        startActivity(new Intent(FirstActivity.this,SecondActivity.class));
+    public void NoPage(View view) {
+        startActivity(new Intent(SecondActivity.this,ThirdActivity.class));
     }
 }
